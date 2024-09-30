449f340f77626a00578a0e3819966c76cc54828d
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-30 13:41:28 +0200

    Parallax effect 🚀

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import NavBar from "../../components/NavBar/NavBar";
 import logoAnimated from "../../assets/images/logoAnimate.svg";
@@ -6,8 +6,30 @@ import logoAnimated from "../../assets/images/logoAnimate.svg";
 import "./Home.css";
 
 export default function Home() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      const x = clientX / window.innerWidth;
+      const y = clientY / window.innerHeight;
+
+      setMousePosition({ x: x * 40, y: y * 7 });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
   return (
-    <section className="home">
+    <section
+      className="home"
+      style={{
+        backgroundPosition: `${mousePosition.x}% ${mousePosition.y}%`,
+      }}
+    >
       <header className="home_header">
         <img
           src={logoAnimated}
