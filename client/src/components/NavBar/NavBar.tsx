6c932ce18eded7885cbce66831dae07067cac3e5
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-09-30 23:07:23 +0200

    Build Navbar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import logoAnimated from "../../assets/images/logoAnimate.svg";
+
 import "./NavBar.css";
 
 export default function NavBar() {
@@ -15,26 +17,29 @@ export default function NavBar() {
   ];
 
   return (
-    <section className="nav">
-      {navElements.map((element) => (
-        <Link
-          to="#"
-          className={`nav_text ${isActive ? "active" : ""}`}
-          key={element.name}
-        >
-          {element.name}
-        </Link>
-      ))}
-      <div className="nav_toggle" onClick={() => setIsActive(!isActive)}>
-        {!isActive ? (
-          <>
-            <p>☰</p>
-            <p>Menu</p>
-          </>
-        ) : (
-          <p>&#215;</p>
-        )}
-      </div>
-    </section>
+    <header className="nav">
+      <img src={logoAnimated} alt="logo Royal Blue" className="nav_img" />
+      <section className="nav_link">
+        {navElements.map((element) => (
+          <Link
+            to="#"
+            className={`nav_link_text ${isActive ? "active" : ""}`}
+            key={element.name}
+          >
+            {element.name}
+          </Link>
+        ))}
+        <div className="nav_link_toggle" onClick={() => setIsActive(!isActive)}>
+          {!isActive ? (
+            <>
+              <p>☰</p>
+              <p>Menu</p>
+            </>
+          ) : (
+            <p>&#215;</p>
+          )}
+        </div>
+      </section>
+    </header>
   );
 }
