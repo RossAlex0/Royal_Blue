dcfbde1984e4dc22c28b956bf1c81bebc719e1a4
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-10-01 11:17:26 +0200

    Created article for see room 💡

--- client/src/pages/Room/Room.tsx
+++ client/src/pages/Room/Room.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import headerRoom from "../../assets/images/headerRoom.svg";
-
+import test from "../../assets/images/headerRoom.svg";
 import "./Room.css";
 
 export default function Room() {
@@ -11,7 +11,21 @@ export default function Room() {
         <div className="room_header_picture" />
         <div className="room_header_grey" />
       </div>
-      <div className="room_carousel"></div>
+      <div className="room_carousel">
+        <article className="room_carousel_article">
+          <img src={test} alt="lit" />
+          <div>
+            <h2>Prestiges</h2>
+            <p>
+              Spacieuses et lumineuses, elles offrent un décor soigné avec des
+              matériaux nobles et des touches modernes. Chaque détail est pensé
+              pour garantir une atmosphère chaleureuse, avec des équipements
+              haut de gamme...
+            </p>
+            <Link to="/">Voir la chambre &#x21d2;</Link>
+          </div>
+        </article>
+      </div>
     </section>
   );
 }
