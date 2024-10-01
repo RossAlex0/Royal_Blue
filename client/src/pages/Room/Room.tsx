57e9dcf1ffc40c94a7fa5138169ba87b3e69df1c
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-10-01 16:40:10 +0200

    Style & use data into room

--- client/src/pages/Room/Room.tsx
+++ client/src/pages/Room/Room.tsx
@@ -1,30 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
+import { LoaderInterface } from "./type";
 import test from "../../assets/images/headerRoom.svg";
+
 import "./Room.css";
 
 export default function Room() {
+  const { roomsData, stylesData } = useLoaderData() as LoaderInterface;
+  console.info(roomsData);
   return (
     <section className="room">
       <div className="room_header">
         <div className="room_header_picture" />
-        <div className="room_header_grey" />
+        <div className="room_header_style">
+          {stylesData.map((style) => (
+            <Link to="#">{style.name}</Link>
+          ))}
+        </div>
       </div>
       <div className="room_carousel">
-        <article className="room_carousel_article">
-          <img src={test} alt="lit" />
-          <div>
-            <h2>Prestiges</h2>
-            <p>
-              Spacieuses et lumineuses, elles offrent un décor soigné avec des
-              matériaux nobles et des touches modernes. Chaque détail est pensé
-              pour garantir une atmosphère chaleureuse, avec des équipements
-              haut de gamme...
-            </p>
-            <Link to="/">Voir la chambre &#x21d2;</Link>
-          </div>
-        </article>
+        {roomsData.map((room) => (
+          <article className="room_carousel_article">
+            <img src={test} alt="lit" />
+            <div>
+              <h2>{room.name}</h2>
+
+              <p>{room.description}</p>
+              <Link to="/">Voir la chambre &#x21d2;</Link>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
   );
