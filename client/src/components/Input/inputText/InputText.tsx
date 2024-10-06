538215cf3160964cabd3e0c88af1a76f44154553
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-10-06 02:00:35 +0200

    Update inputText

--- client/src/components/Input/inputText/InputText.tsx
+++ client/src/components/Input/inputText/InputText.tsx
@@ -4,6 +4,7 @@ import { ToolsInterface } from "./type";
 
 import visible from "../../../assets/icon/visible.svg";
 import notVisible from "../../../assets/icon/notVisible.svg";
+import "./inputText.css";
 
 export default function InputText({ tools }: { tools: ToolsInterface }) {
   const [isVisible, setIsVisible] = useState<boolean>(false);
@@ -11,27 +12,31 @@ export default function InputText({ tools }: { tools: ToolsInterface }) {
   return (
     <>
       <label className="inputText_label">{tools.label}</label>
-      <input
-        type={tools.type}
-        placeholder={tools.placeholder}
-        value={tools.state}
-        onChange={(e) => tools.setter(e.target.value)}
-        className="inputText_input"
-      />
-      {tools.type === "password" &&
-        (!isVisible ? (
-          <img
-            src={notVisible}
-            alt="Close eyes"
-            onClick={() => setIsVisible(true)}
-          />
-        ) : (
-          <img
-            src={visible}
-            alt="Open eyes"
-            onClick={() => setIsVisible(false)}
-          />
-        ))}
+      <div className="inputText_container">
+        <input
+          type={tools.type}
+          placeholder={tools.placeholder}
+          value={tools.state}
+          onChange={(e) => tools.setter(e.target.value)}
+          className="inputText_container_input"
+        />
+        {tools.type === "password" &&
+          (!isVisible ? (
+            <img
+              src={notVisible}
+              alt="Close eyes"
+              onClick={() => setIsVisible(true)}
+              className="inputText_container_image"
+            />
+          ) : (
+            <img
+              src={visible}
+              alt="Open eyes"
+              onClick={() => setIsVisible(false)}
+              className="inputText_image"
+            />
+          ))}
+      </div>
     </>
   );
 }
