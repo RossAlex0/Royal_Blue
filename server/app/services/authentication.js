6992a7ca6327c751074dc8ea84eba348581fbe85
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-10-06 23:35:04 +0200

    Update middleware checkpassword

--- server/app/services/authentication.js
+++ server/app/services/authentication.js
@@ -6,14 +6,26 @@ const checkPassword = async (req, res, next) => {
       mail: req.body.mail,
       password: req.body.password,
     };
+
     const costumer = await tables.costumer.readByEmail(user.mail);
+
     if (!costumer) {
       throw new Error("Véréfié votre adresse e-mail.");
     }
+
+    req.user = {
+      id: costumer.id,
+      lastname: costumer.lastname,
+      firstname: costumer.firstname,
+      email: costumer.email,
+      phone: costumer.phone,
+      country: costumer.country,
+    };
+
     if (costumer.password === user.password) {
       next();
     } else {
-      throw new Error("Véréfié votre mot de passe.");
+      throw new Error("Vérifié votre mot de passe");
     }
   } catch (err) {
     next(err);
