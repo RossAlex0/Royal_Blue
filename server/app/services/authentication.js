f65f6d43e0c517995f90e971a7c0a33de757c7c5
Alex Rossignol <alexrossignol@air-de-marlene.home>
2024-10-07 15:58:31 +0200

    Created function for hashed password

--- server/app/services/authentication.js
+++ server/app/services/authentication.js
@@ -32,4 +32,25 @@ const checkPassword = async (req, res, next) => {
   }
 };
 
-module.exports = { checkPassword };
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 19 * 2 ** 10,
+  timeCost: 2,
+  parallelism: 1,
+};
+
+const hashPassword = async (req, res, next) => {
+  try {
+    const { password } = req.body;
+
+    const hashedPassword = await argon2.hash(password, hashingOptions);
+
+    req.body.password = hashedPassword;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { checkPassword, hashPassword };
